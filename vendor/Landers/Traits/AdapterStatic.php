068964b80194e04262f1fce19e95098a44d200d4
陆海兴 <luhaixing@qq.com>
2016-03-16 18:01:56 +0800

    ...

--- vendor/Landers/Traits/AdapterStatic.php
+++ vendor/Landers/Traits/AdapterStatic.php
@@ -4,7 +4,7 @@ namespace Landers\Traits;
 Trait AdapterStatic {
     public static function __callStatic($method, $args) {
         if (!$class = self::$adapter) {
-            throw new Exception('未执行'.__CLASS__.'初始化！');
+            throw new \Exception('未执行'.static::class.'初始化！');
         }
         switch (count($args)) {
             case 0: return $class::$method();

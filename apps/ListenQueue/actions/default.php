2211c18bbbb2e97ecb1b4b6b0a00665c973df47c
陆海兴 <luhaixing@qq.com>
2016-06-28 14:45:16 +0800

    ...

--- apps/ListenQueue/actions/default.php
+++ apps/ListenQueue/actions/default.php
@@ -8,7 +8,7 @@ $queue = System::argv(3);
 if (!$queue) System::halt('未指定队列名称！');
 
 $config = Config::get('queue', $queue);
-if (!$config) System::halt('不存在队列标识“%s”', $queue);
+if (!$config) System::halt(sprintf('不存在队列标识“%s”', $queue));
 
 $title = sprintf('【%s监听器】（%s）开始工作', $config['name'], System::app('name'));
 Response::note([$title,'#dbline']);

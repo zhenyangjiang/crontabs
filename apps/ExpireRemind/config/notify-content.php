149ddccbb28104f0e6f42c9f9508629a511147ec
陆海兴 <luhaixing@qq.com>
2016-06-30 18:13:10 +0800

    ...

--- apps/ExpireRemind/config/notify-content.php
+++ apps/ExpireRemind/config/notify-content.php
@@ -6,7 +6,7 @@ return [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：'.PHP_EOL.'　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，由于您的余额不足以自动续费下个月的费用，请尽快充值，以免服务受影响。',
         ],
-        'message' => [
+        'inner' => [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：'.PHP_EOL.'　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，由于您的余额不足以自动续费下个月的费用，请尽快充值，以免服务受影响。',
         ],
@@ -19,7 +19,7 @@ return [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：'.PHP_EOL.'　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，请尽快充值并续费，以免服务受影响。',
         ],
-        'message' => [
+        'inner' => [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：<br/>　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，请尽快充值并续费，以免服务受影响。',
         ],
@@ -32,7 +32,7 @@ return [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：'.PHP_EOL.'　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，您的余额足够支付下个月的费用，请尽快登录系统进行续费操作，以免服务受影响。',
         ],
-        'message' => [
+        'inner' => [
             'title'   => '云主机过期续费提醒',
             'content' => '尊敬的用户{user_name}：<br/>　　您的云主机：{instance_name}（IP：{instance_ip}）将于{days}天后({expire_date})到期，您的余额足够支付下个月的费用，请尽快登录系统进行续费操作，以免服务受影响。',
         ],

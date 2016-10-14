fefb5ad0e12510628efc54f4fc4fb01e3fa5ce20
Landers <luhaixing@qq.com>
2016-10-14 16:11:15 +0800

    no message

--- apps/Tester/actions/test.php
+++ apps/Tester/actions/test.php
@@ -4,4 +4,15 @@ use Landers\Framework\Core\Response;
 include 'inc-headline.php';
 
 
+while (true) {
+    Response::note('repository(\'tester\')->index() = ');
+    try {
+        $ret = repository('tester')->index();
+        Response::echoText($ret);
+    } catch (\Exception $e) {
+        System::halt();
+    }
+    sleep(1);
+}
+
 System::complete();
